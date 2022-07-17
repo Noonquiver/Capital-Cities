a818a6d66bdc1d78c595b621a6e81da2e6e779cd
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-16 21:41:27 -0500

    first commit

---
+++ Capital Cities/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Capital Cities
+//
+//  Created by Camilo Hernández Guerrero on 16/07/22.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
