bb894b9746562634cb2d3eecbf525338bb98fe9e
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-16 23:01:36 -0500

    Action sheet to select map types added.

--- Capital Cities/ViewController.swift
+++ Capital Cities/ViewController.swift
@@ -14,6 +14,34 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let alertController = UIAlertController(title: "Select a map type", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Standard", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .standard
+        }))
+        alertController.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .hybrid
+        }))
+        alertController.addAction(UIAlertAction(title: "Hybrid Flyover", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .hybridFlyover
+        }))
+        alertController.addAction(UIAlertAction(title: "Muted Standard", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .mutedStandard
+        }))
+        alertController.addAction(UIAlertAction(title: "Satellite", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .satellite
+        }))
+        alertController.addAction(UIAlertAction(title: "Satellite Flyover", style: .default, handler: {
+            [weak self] _ in
+            self?.mapView.mapType = .satelliteFlyover
+        }))
+        
+        present(alertController, animated: true)
+        
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
@@ -21,6 +49,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
         
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
     }
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
